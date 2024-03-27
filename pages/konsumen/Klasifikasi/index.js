b4a6ef7be05ae525c2a88c64e19b6bf6876261da
anti009x <riskisuleman76@gmail.com>
2024-03-27 09:11:29 +0700

    Update Resposife Design + update Transaksi

--- pages/konsumen/Klasifikasi/index.js
+++ pages/konsumen/Klasifikasi/index.js
@@ -1,12 +1,14 @@
-import { Button, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import { Button, StyleSheet, Text, TouchableOpacity, View, Image, Dimensions } from 'react-native'
 import React from 'react'
+
+const windowWidth = Dimensions.get('window').width;
 
 export default function KlasifikasiObjek() {
   return (
     <View style={styles.container}>
         <View style={styles.kamera}>
-        <Image source={require('../../img/camera.png')} style={styles.camera}/>
-        <Text style={styles.txt}>Camera</Text>
+            <Image source={require('../../img/camera.png')} style={styles.camera}/>
+            <Text style={styles.txt}>Camera</Text>
         </View>
 
         <View style={styles.fotocontainer}>
@@ -14,131 +16,104 @@ export default function KlasifikasiObjek() {
                 <Image source={require('../../img/Kursi.png')} style={styles.img}/>
             </View>
         </View>
-            <View style={styles.resultcontainer}>
-                <View style={styles.result}>
-                    <Text style={styles.textresul}>
-                    Deskripsi : Barang Direkmendasikan ke Mobil   </Text>
-                    <Text style={styles.textresul}>Percentase : 80%</Text>
-                   <Text style={styles.textresul}>Nama : Kursi</Text>
-                   <Text style={styles.textresul}>Klarifikasi : Berat </Text>
-                </View>
+        <View style={styles.resultcontainer}>
+            <View style={styles.result}>
+                <Text style={styles.textresul}>
+                    Deskripsi : Barang Direkomendasikan ke Mobil
+                </Text>
+                <Text style={styles.textresul}>Percentase : 80%</Text>
+                <Text style={styles.textresul}>Nama : Kursi</Text>
+                <Text style={styles.textresul}>Klasifikasi : Berat</Text>
             </View>
-            <TouchableOpacity>
+        </View>
+        <TouchableOpacity>
             <View style={styles.button}>
-                    <Text style={{color:'white',textAlign:'center'}}>Kirim Sekarang !</Text>
-                </View>
-                </TouchableOpacity>
+                <Text style={{color:'white',textAlign:'center'}}>Kirim Sekarang !</Text>
+            </View>
+        </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
     button:{
         marginTop: 15,
-        display: 'flex',
         backgroundColor: 'black',
-        width: 120,
+        width: windowWidth * 0.3,
         padding: 8,
-        alignSelf:'flex-end',
+        alignSelf: 'flex-end',
+        borderRadius: 5,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
-
-
-        
     },
-
     resultcontainer:{
         backgroundColor:'#FFFFFF',
-
-        // alignContent:'center',
-        // flexDirection:'row-reverse',
-        padding:20,
-        marginTop:10,
-        borderRadius:20,
+        padding: 20,
+        marginTop: 10,
+        borderRadius: 20,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
-
     },
     result:{
-        flexDirection:'column',
-        // justifyContent:'center',
-        // alignContent:'center',
-        alignItems:'center'
+        alignItems: 'center'
     },
     textresul:{
-            fontWeight:'bold',
-            fontSize:20,
-            color:'black',
-            
-
+        fontWeight:'bold',
+        fontSize: 16,
+        color:'black',
     },
     fotocontainer:{
-        marginTop:15,
+        marginTop: 15,
         backgroundColor:'#D9D9D9',
-        padding:15,
-        // paddingBottom:400
-        // width:530
-        borderRadius:10,
+        padding: 15,
+        borderRadius: 10,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
-        alignContent: 'space-between',
-
     },
     foto:{
         backgroundColor:'#FFFFFF',
         justifyContent:'center',
-        // alignContent:'center',
-        flexDirection:'row-reverse',
-        padding:0.1,
-        paddingBottom:0.1,
-        borderRadius:10,
+        padding: 10,
+        borderRadius: 10,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
-
     },
     container: {
         flex: 1, 
         backgroundColor: '#EDA01F',
         padding: 10,
-      },
-      kamera:{
+    },
+    kamera:{
         justifyContent:'center',
-        alignContent:'center',
-        flexDirection:'column',
         alignItems: 'center',
-
-      },
-      kameratext:{
-            fontWeight:'bold',
-            fontSize:20,
-            color:'black'
-      },
-      img:{
-        width: 670,
-        height: 800,
-        borderRadius:10,
-      },
-      camera:{
-        height:150,
-        width:150,
-        marginTop: -30
-      },
-      txt:{
+    },
+    camera:{
+        height: windowWidth * 0.35,
+        width: windowWidth * 0.35,
+        marginTop : -60
+    },
+    txt:{
         fontWeight:'bold',
         fontSize: 20,
-        marginTop: -40
-      }
+        marginTop : -60
+
+    },
+    img:{
+        width:450 ,
+        height: 450,
+        borderRadius: 10,
+        alignSelf:'center'
+    }
 })
--- pages/konsumen/Transaksi/index.js
+++ pages/konsumen/Transaksi/index.js
@@ -6,33 +6,124 @@ export default function Transaksi() {
   const navigation =  useNavigation();
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Pilih Paket yang Anda Mau:</Text>
+      <Text style={styles.pilihpaket}>Pilih Paket yang Anda Mau:</Text>
       <View style={styles.cardPaket}>
         <View style={styles.cardPaket1}>
           <View style={styles.cardPaketChild}>
+            <Text style={styles.textpaket}>Logo</Text>
+            <Text style={styles.textpaket}>Paket Standar</Text>
           </View>
           <View style={styles.cardPaketChild}>
+            <Text style={styles.textpaket}>Logo</Text>
+            <Text style={styles.textpaket}>Paket Reguler</Text>
           </View>
         </View>
         <View style={styles.cardPaket2}>
           <View style={styles.cardPaketChild}>
+            <Text style={styles.textpaket}>Logo</Text>
+            <Text style={styles.textpaket}>Paket Cepat</Text>
           </View>
         </View>
       </View>
+      {/* Deskripsi Paket */}
+      <View style={styles.paket}>
+          <View>
+            <Text style={styles.textpaket2}>1X</Text>
+          </View>
+          <View>
+            <Text style={styles.textpaket1}>
+            Paket Standar yang sangat mantap
+            </Text>
+          </View>
+          <Text style={styles.textpaket1}>
+              Rp35.000
+          </Text>
+          <View>
+          </View>
+      </View>
+            {/* Checkout */}
+      <View style={styles.checkout}>
+        <View>
+          <Text style={styles.textpaket4}>
+                  Jumlah
+          </Text>
+          <Text style={styles.textpaket4}>
+                 Rp350.000
+            </Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={{color:'white' , fontWeight:'bold' ,justifyContent:'center' ,alignSelf:'center'}}>
+            Checkout
+          </Text>
+          </View>
+      </View>
     </View>
-    
   )
 }
 
 const styles = StyleSheet.create({
+  pilihpaket:{
+    color:'black' ,
+    marginTop:20 , 
+    fontWeight:'bold',
+    fontStyle:'italic' , 
+    fontSize:19 , 
+    marginLeft:20
+  },
+  button:{
+    backgroundColor:'#EDA01F',
+    padding:10,
+    // width:110,
+    alignSelf:'flex-end'
+  },
+  checkout:{
+    marginTop:30,
+    backgroundColor:'#0B111F',
+    padding:10,
+    // flex:1
+
+  },
+  paket:{
+    backgroundColor:'#0B111F',
+    padding:30,
+    marginTop:180,
+    // flex:1
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  textpaket1:{
+    color:'white',
+    // textAlign:'center'
+    padding:10
+  },
+  textpaket4:{
+    color:'white',
+    // textAlign:'center'
+    // padding:
+    // marginTop:-1000
+      fontSize:20
+  },
+  textpaket2:{
+    color:'white',
+    // textAlign:'center'
+    backgroundColor:'#EDA01F',
+    padding:10,
+    borderRadius:20,
+    fontWeight:'bold'
+  },
+  textpaket:{
+    color:'white',
+    textAlign:'center'
+  },
   container: {
     flex: 1,
     backgroundColor: '#eda01f',
-    padding: 20
+    // padding: 10,
+
   },
   cardPaket:{
     backgroundColor: '#0b111f',
-    borderRadius: 25,
+    borderRadius: 10,
     padding: 20,
     marginTop: 20,
     shadowColor: '#000',
@@ -41,8 +132,10 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
     alignContent: 'center',
-    alignItems: 'center'
-    
+    alignItems: 'center',
+    paddingBottom:40,
+    marginLeft:10,
+    marginRight:10
   },
   cardPaket1:{
     backgroundColor: '#0b111f',
@@ -59,18 +152,25 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontStyle: 'italic',
     fontWeight:'bold',
+    color:'black'
   },
   cardPaketChild:{
-    backgroundColor: 'yellow',
-    marginBottom: 10,
-    flexDirection: 'row',
-    width: 10, 
-    height: 10, 
+    backgroundColor: '#000000',
+    // marginBottom: 10,
+    // flexDirection: 'row',
+    flexDirection:'column',
+    justifyContent:'space-around',
+    // width: 10, 
+    // height: 10, 
     borderRadius: 10,
-    color: 'white',
-    marginLeft:15,
-    marginRight: 15,
-    marginTop:30,
-    padding: 55,
+    // color: 'white',
+    // marginLeft:15,
+    // marginRight: 15,
+    // marginTop:30,
+    // alignSelf:'center',
+    marginLeft:10,
+    marginTop:15,
+    padding:40,
+    // marginBottom:40
   }
 })
