a3ec205f9b54480ec795e5c8f0029e6779a8d24c
Sanorikuy <sanorikuy@gmail.com>
2024-03-28 14:58:00 +0700

    Tambah icon

---
+++ pages/konsumen/Kurir/index.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Kurir() {
+  return (
+    <View>
+      <Text>Kurir</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
