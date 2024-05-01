9efe17ed28d3ec02fd566fea5007465889243e21
anti009x <riskisuleman76@gmail.com>
2024-05-01 17:06:12 +0700

    ClientPatch1.1

--- pages/Kurir/ChattingKonsumen/index.js
+++ pages/Kurir/ChattingKonsumen/index.js
@@ -1,12 +1,270 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Dimensions, ScrollView } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import socket from '../../socket';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { baseUrl } from '../../baseUrl';
 
+const { width } = Dimensions.get('window');
 export default function ChattingKonsumen() {
-  return (
-    <View>
-      <Text>ChattingKonsumen</Text>
-    </View>
-  )
-}
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState('');
+  const [ambilDataProfile, setAmbilDataProfile] = useState([]);
 
-const styles = StyleSheet.create({})
+  const [dataPribadi,setDataPribadi]=useState({});
+
+  const [AmbilPesan,AmbilPesanMasuk]= useState ([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        const response = await axios(`${baseUrl.url}/datauser`,{
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          method: "GET"
+        });
+        setAmbilDataProfile(response.data["data"]);
+      //   console.log(response.data)
+
+      //  //lu cobain dulu dah console.log ada kgk datanya 
+        console.log(response.data) 
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [dataPribadi.token]);
+
+  useEffect(() => {
+    const messageListener = (message) => {
+      setMessages(previousMessages =>
+        GiftedChat.append(previousMessages, {
+          ...message,
+          createdAt: new Date(message.createdAt),
+        })
+      );
+    };
+
+    socket.on('message', messageListener);
+
+
+    return () => {
+      socket.off('message', messageListener);
+    };
+  }, []);
+
+
+  const onSend = useCallback(async (newMessages = []) => {
+    if (ambilDataProfile.nama && newMessages.length > 0) {
+      const messageText = newMessages[0].text.trim();
+      if (messageText.length > 0) {
+        const newMessage = {
+          text: messageText,
+          createdAt: new Date(),
+          Nama: ambilDataProfile.nama
+        };
+  
+        setText('');
+        socket.emit('message', newMessage);
+  
+
+        await new Promise(resolve => {
+          socket.once('messageSent', () => {
+
+            resolve();
+          });
+        });
+
+
+        setMessages(previousMessages =>
+          GiftedChat.append(previousMessages, newMessage)
+        );
+      }
+
+    }
+  }, [ambilDataProfile]);
+
+  const onChangeText = (inputText) => {
+    setText(inputText);
+    // resolve();
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.main}>
+        { ambilDataProfile && (
+          <View style={styles.cardInfo}>
+            <View style={styles.cardInfoRow}>
+              <Image source={require('../../img/logo.png')} style={styles.logo} />
+              <View style={{ flex: 1 }}>
+                <Text style={styles.text}>{ambilDataProfile.nama}</Text>
+                <Text style={styles.text}>{ambilDataProfile.alamat}</Text>
+              </View>
+              <View style={{ alignItems: 'flex-end' }}>
+                <Text style={{ color: '#EDA01F' }}>{ambilDataProfile.nohp}</Text>
+              </View>
+            </View>
+          </View>
+              ) }
+ 
+          <ScrollView>
+          <View style={styles.resultcontainer}>
+          {messages.map((message, index) => (
+              <View key={index} style = {styles.result1}>
+                <Text style = {styles.text}>{message.Nama}</Text>
+                <Text style = {styles.text}>{message.text}</Text>
+              </View>
+                 ))}
+            </View>
+            </ScrollView>
+{/*   
+ 
+            <GiftedChat
+      messages={messages}
+      onSend={(newMessages) =>
+        setMessages((previousMessages) =>
+          GiftedChat.append(previousMessages, newMessages),
+        )
+      }
+      user={{ _id: 1 }}
+    /> */}
+    
+          <View style={styles.chatbar}>
+                <TextInput
+              style={[styles.text2, styles.input, styles.form2, { color: 'black' }]}
+              placeholder="Ketik Pesan"
+              multiline={true}
+              numberOfLines={4}
+              placeholderTextColor="rgba(255, 255, 255, 0.5)"
+              onChangeText={onChangeText}
+              value={text}
+            />
+       <TouchableOpacity onPress={() => onSend([{ text }])} style={styles.buttonsend}>
+        <Image source={require('../../img/Send-Message.png')} style={styles.sendbuttonicon} />
+      </TouchableOpacity>
+   
+          </View>
+     
+        </View>
+      </View>
+
+    </View>
+
+  );
+}
+const styles = StyleSheet.create({
+text:{color: 'white'},
+  resultcontainer: {
+    flexDirection: 'column',
+    marginTop: 40,
+  },
+  text2:{
+    color:'black',
+    fontWeight:'bold'
+
+  },
+  result1: {
+    backgroundColor: 'black',
+    padding: 30,
+    borderRadius: 10,
+    alignItems: 'flex-start',
+    alignContent: 'flex-start',
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: width * 0.30, 
+  },
+  result2: {
+    backgroundColor: 'black',
+    padding: 30,
+    borderRadius: 10,
+    alignItems: 'flex-start',
+    alignContent: 'flex-start',
+    marginLeft: width * 0.30, 
+    marginRight: 20,
+    marginTop: 20,
+  },
+  result3: {
+    backgroundColor: 'black',
+    padding: 30,
+    borderRadius: 10,
+    alignItems: 'flex-end',
+    alignContent: 'flex-end',
+    justifyContent: 'flex-end',
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: width * 0.30, 
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  cardInfo: {
+    backgroundColor: '#0B111F',
+    padding: 15,
+  },
+  cardInfoRow: {
+    marginRight: 40,
+    marginLeft: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#EDA01F',
+    padding: 10,
+  },
+  header: {
+    marginTop: 15,
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 10,
+  },
+  main: {
+    backgroundColor: '#FFFFFF',
+    marginTop: 15,
+    height: 800,
+  },
+  chatbar: {
+    marginRight: 20,
+    marginLeft: 20,
+    // marginTop:300,
+    flexDirection: "row",
+  },
+  input: {
+    height: 40,
+    width: 400,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    
+
+  },
+  form2: {
+    backgroundColor:'#d9d9d9',
+    color:'white'
+  },
+  buttonsend:{
+    backgroundColor:'pink',
+    width:40,
+    height:40,
+    justifyContent: 'space-between',
+    marginLeft:10,
+    borderRadius:100,
+    alignItems: 'center',
+  },
+  sendbuttonicon:{
+    width:40,
+    height: 40,
+    borderRadius: 3,
+    marginLeft: 5,
+    alignSelf:'center',
+  }
+});
--- pages/Kurir/Gaji/index.js
+++ pages/Kurir/Gaji/index.js
@@ -1,34 +1,274 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity, Image, ScrollView } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { baseUrl } from '../../baseUrl';
 
 export default function Gaji() {
   return (
     <View style={styles.container}>
 
       <View style={styles.box}>
-        <Text>Gaji</Text>
+        <View style={styles.form1}>
+          <View style={styles.imagconatiner}>
+            <Image
+              source={require('../../img/Countdown.png')}
+              resizeMode="cover"
+              style={styles.img}
+            />
+          </View>
+     
+          <View style={styles.textContainer}>
+            <Text style={styles.Text}>TOTAL PENDAPATAN</Text>
+            <Text style={styles.Text1}>Rp.50000</Text>
+          </View>
+        </View>
+
+        <View style={styles.submit}>
+          <Image
+              source={require('../../img/money.png')}
+              resizeMode="cover"
+              style={styles.img2}
+            />
+          <View style={styles.submitbutton}>
+            <Text style={styles.text3}>
+              TARIK SEKARANG !
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.form2}>
+          <Text style={styles.Text}>
+            LOG PENARIKAN
+          </Text>
+          <ScrollView>
+            <View style={styles.log}>
+              <View style={styles.riwayat}>
+                <Text style={styles.textlog}>
+                  5-1-2024 : Rp.2000
+                </Text>
+                <Text style={styles.textlog}>
+                  5-1-2024 : Rp.2000
+                </Text>
+                <Text style={styles.textlog}>
+                  5-1-2024 : Rp.2000
+                </Text>
+                <Text style={styles.textlog}>
+                  5-1-2024 : Rp.2000
+                </Text>
+                <Text style={styles.textlog}>
+                  5-1-2024 : Rp.2000
+                </Text>
+                <Text style={styles.textlog}>
+                  5-1-2024 : Rp.2000
+                </Text>
+                <Text style={styles.textlog}>
+                  5-1-2024 : Rp.2000
+                </Text>
+                <Text style={styles.textlog}>
+                  5-1-2024 : Rp.2000
+                </Text>
+                <Text style={styles.textlog}>
+                  5-1-2024 : Rp.2000
+                </Text>
+              </View>
+            </View>
+          </ScrollView>
+        </View>
+      </View>
+
+      <View style={styles.containerdropdown}>
+        <View style={{ marginLeft: 30 }}>
+          <Image
+            source={require('../../img/ArrowUp.png')}
+            resizeMode="cover"
+            style={styles.img3}
+          />
+        </View>
+        <View style={{ marginRight: 30 }}>
+          <Image
+            source={require('../../img/ArrowDown.png')}
+            resizeMode="cover"
+            style={styles.img3}
+          />
+        </View>
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#EDA01F',
-        // alignItems: 'center',
-        // justifyContent: 'center',
-    },
-    box: {
-        // flex: 1,
-        backgroundColor: '#0B111F',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 100,
-        marginBottom: 100,
-        // marginLeft: 100,
-        // marginRight: 100,
-        borderRadius: 3,
-    }
+  img3: {
+    width: 100,
+    height: 100
+  },
+  containerdropdown: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: -190
+  },
+  textlog: {
+    color: 'red',
+    fontSize: 18
+  },
+  riwayat: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingLeft: 60,
+    paddingRight: 60
+  },
+  log: {
+    backgroundColor: '#0B111F',
+    padding: 10,
+    borderRadius: 4,
+    marginHorizontal: 1
+  },
+  text3: {
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  submit: {
+    marginTop: -110,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  submitbutton: {
+    marginTop: 60,
+    marginLeft: 170,
+    padding: 5,
+    backgroundColor: '#EDA01F',
+    height: 30,
+    borderRadius: 4,
+  },
+  Text: {
+    color: 'black',
+    textAlign: 'center',
+    fontFamily: 'verdana',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  Text1: {
+    color: 'black',
+    textAlign: 'center',
+    fontFamily: 'verdana',
+    fontWeight: 'bold',
+    marginTop: 30,
+    fontSize: 25
+  },
+  textContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: -25
+  },
+  imagconatiner: {
+    marginTop: -100
+  },
+  img: {
+    width: 400,
+    height: 120,
+    alignSelf: 'center',
+  },
+  img2: {
+    width: 100,
+    height: 100,
+    alignSelf: 'flex-start'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#EDA01F',
+  },
+  box: {
+    backgroundColor: '#0B111F',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100,
+    borderRadius: 3,
+    marginLeft: 30,
+    marginRight: 30,
+    justifyContent: 'center',
+    paddingBottom:20
+  },
+  form1: {
+    backgroundColor: '#FFFF',
+    padding: 10,
+    width: 450,
+    height: 350,
+    marginTop: 20,
+    marginBottom: 5,
+    borderRadius: 3,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  form2: {
+    backgroundColor: '#FFFF',
+    padding: 10,
+    width: 450,
+    height: 250,
+    marginTop: 20,
+    marginBottom: 5,
+    borderRadius: 3,
+    alignSelf: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
 })
-
+//   },
+//   imagconatiner: {
+//     marginTop: -height * 0.1
+//   },
+//   img: {
+//     width: width * 0.95,
+//     height: height * 0.12,
+//     alignSelf: 'center',
+//   },
+//   img2: {
+//     width: width * 0.25,
+//     height: height * 0.1,
+//     alignSelf: 'flex-start'
+//   },
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#EDA01F',
+//   },
+//   box: {
+//     backgroundColor: '#0B111F',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     marginTop: height * 0.1,
+//     borderRadius: 3,
+//     marginLeft: width * 0.07,
+//     marginRight: width * 0.07,
+//     justifyContent: 'center',
+//   },
+//   form1: {
+//     backgroundColor: '#FFFF',
+//     padding: 10,
+//     width: width * 0.95,
+//     height: height * 0.35,
+//     marginTop: 20,
+//     marginBottom: 5,
+//     borderRadius: 3,
+//     flexDirection: 'column',
+//     justifyContent: 'center',
+//     alignContent: 'center',
+//     alignItems: 'center',
+//     alignSelf: 'center',
+//   },
+//   form2: {
+//     backgroundColor: '#FFFF',
+//     padding: 10,
+//     width: width * 0.95,
+//     height: height * 0.25,
+//     marginTop: 20,
+//     marginBottom: 5,
+//     borderRadius: 3,
+//     alignSelf: 'center',
+//     flexDirection: 'column',
+//     alignItems: 'center'
+//   }
+// })
