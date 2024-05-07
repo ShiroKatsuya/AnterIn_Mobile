731b10f78d1ea1a3e8a0046679d23fcee7d90c66
Rizky Sulaeman <95662101+anti009x@users.noreply.github.com>
2024-05-07 15:54:49 +0700

    Update baseUrl.js

--- pages/baseUrl.js
+++ pages/baseUrl.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 export const baseUrl = {
     url: "http://192.168.100.56:8888/api",
-
+    //For Production
     // url: "https://anterin-web-production.up.railway.app/api", 
 
 
